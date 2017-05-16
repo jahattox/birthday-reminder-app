21899b1854e56b02b408594b2a44061dbaba7758
James Austin Hattox <jaustinhattox@gmail.com>
2017-05-16 20:13:26 +0100

    Add karma.conf.js file

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+const webpackConfig = require('./webpack.config.js');
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['Chrome'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: [
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'app/tests/**/*.test.js'
+    ],
+    preprocessors: {
+      'app/tests/**/*.test.js': ['webpack', 'sourcemap']
+    },
+    reporters: ['mocha'],
+    client: {
+      mocha: {
+        timeout: '5000'
+      }
+    },
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
